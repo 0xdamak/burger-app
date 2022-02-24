005fbedae4b6f3a10941347c3156f92a4295f7ac
damak <akinlade.co@gmail.com>
2022-02-24 15:38:19 +0100

    app routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import Home from "./pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Live from "./pages/LiveApp/Live";
+import BurgerBuilder from "./pages/LiveApp/BurgerBuilder/BurgerBuilder";
+import Orders from "./pages/LiveApp/Orders/Orders";
+import Login from "./pages/Login/Login";
 
 import "./App.css";
 
@@ -9,6 +13,12 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="app" element={<Live />}>
+          <Route path="" element={<BurgerBuilder />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </div>
   );
