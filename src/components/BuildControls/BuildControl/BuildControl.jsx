08a76bdd498bb8a7b5380b6b83bd76f36e350005
damak <akinlade.co@gmail.com>
2022-02-24 11:02:07 +0100

    added ingredient amount to label

--- src/components/BuildControls/BuildControl/BuildControl.jsx
+++ src/components/BuildControls/BuildControl/BuildControl.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Classes from "./BuildControl.module.css";
 
-function BuildControl({ add, remove, label }) {
+function BuildControl({ add, remove, label, amount }) {
   return (
     <div className={Classes.Wrapper}>
       <div className={Classes.Label}>
-        <h4>{label}</h4>
+        <h4>
+          {label}: <strong>{amount}</strong>
+        </h4>
       </div>
       <div className={Classes.Buttons}>
         <button onClick={remove}>-</button>
--- src/components/BuildControls/BuildControls.jsx
+++ src/components/BuildControls/BuildControls.jsx
@@ -1,20 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import BuildControl from "./BuildControl/BuildControl";
 
 import Classes from "./BuildControls.module.css";
 
-function BuildControls({ reset, order }) {
+function BuildControls({ reset, order, add, remove, amount }) {
+  const burger = useSelector((state) => state.burgerBuilder.ingredients);
+
+  const buildControls = [
+    { label: "Salad", id: "salad" },
+    { label: "Cheese", id: "cheese" },
+    { label: "Meat", id: "meat" },
+    { label: "Tomato", id: "tomato" },
+    { label: "Onion", id: "onion" },
+    { label: "Pear", id: "pear" },
+  ];
+
+//   console.log(burger);
+
   return (
     <div className={Classes.Wrapper}>
       <h3 className={Classes.Price}>Price: $10</h3>
       <div className={Classes.Controls}>
-        <BuildControl label="Meat" />
-        <BuildControl label="Meat" />
-        <BuildControl label="Meat" />
-        <BuildControl label="Meat" />
-        <BuildControl label="Meat" />
-        <BuildControl label="Meat" />
-        <BuildControl label="Meat" />
+        {buildControls.map((buildControl, index) => {
+          return (
+            <BuildControl
+              add={() => add(buildControl.id)}
+              remove={() => remove(buildControl.id)}
+              key={buildControl.id}
+              label={buildControl.label}
+              amount={burger[buildControl.id].amount}
+            />
+            // console.log(burger[buildControl.id].amount)
+          );
+        })}
       </div>
       <div className={Classes.Buttons}>
         <button onClick={reset}>Reset</button>
