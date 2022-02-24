abff5795f868c079c963caec7707fbca28f66b73
damak <akinlade.co@gmail.com>
2022-02-24 15:35:07 +0100

    burger update (Start Adding Ingredients text)

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -7,24 +7,35 @@ import "./Burger.css";
 function Burger({ ingredients }) {
   const ingredientsArray = Object.keys(ingredients);
 
-  let _burger = ingredientsArray.map((ingredient) => {
-    return [...Array(ingredients[ingredient].value)].map((_, index) => {
-      return (
-        <BurgerIngredient
-          key={ingredient + index}
-          ingredientType={ingredient}
-          className={ingredients[ingredient].id}
-        />
-      );
-    });
-  });
+  let _burger = ingredientsArray
+    .map((ingredient) => {
+      return [...Array(ingredients[ingredient].number)].map((_, index) => {
+        return (
+          <BurgerIngredient
+            key={ingredient + index}
+            ingredientType={ingredient}
+            className={`${ingredients[ingredient].id}`}
+          />
+        );
+      });
+    })
+    .flat(1);
+
+  console.log(_burger);
+
   return (
-    <div className={Classes.Container}>
+    <div className={Classes.Wrapper}>
       <div className="Burger">
         <BurgerIngredient ingredientType="top-bun" className="TopBun" />
         {_burger}
         <BurgerIngredient ingredientType="bottom-bun" className="BottomBun" />
       </div>
+
+      {_burger.length === 0 && (
+        <div className={Classes.Text}>
+          <h4>Start Adding Ingredients</h4>
+        </div>
+      )}
     </div>
   );
 }
