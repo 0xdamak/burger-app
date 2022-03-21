89be13c63522204e0ae99339f13163394c8b9793
damak <akinlade.co@gmail.com>
2022-03-21 16:50:19 +0100

    export orders page from index file, css => scss

---
+++ src/pages/Orders/Orders.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import OrderList from "../../components/OrderCard/OrderCard";
+import Classes from "./Orders.module.scss";
+
+function Orders() {
+  const [activeTab, setActiveTab] = useState("open orders");
+
+  const openOrders = (
+    <React.Fragment>
+      <OrderList />
+      <OrderList />
+      <OrderList />
+      <OrderList />
+      <OrderList />
+    </React.Fragment>
+  );
+
+  const closedOrders = (
+    <React.Fragment>
+      <OrderList />
+      <OrderList />
+    </React.Fragment>
+  );
+
+  return (
+    <div className={Classes.Wrapper}>
+      <div className={Classes.Header}>
+        <h3>Orders</h3>
+      </div>
+      <div className={Classes.Body}>
+        <div className={Classes.Tabs}>
+          <h4
+            onClick={() => setActiveTab("open orders")}
+            className={
+              activeTab === "open orders" ? Classes.ActiveTab : undefined
+            }
+          >
+            Open Orders(28)
+          </h4>
+          <h4
+            onClick={() => setActiveTab("closed orders")}
+            className={
+              activeTab === "closed orders" ? Classes.ActiveTab : undefined
+            }
+          >
+            Closed Orders(20)
+          </h4>
+        </div>
+        <div className={Classes.OrderList}>
+          {activeTab === "open orders" && openOrders}
+          {activeTab === "closed orders" && closedOrders}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
