9485729c8f6f95a8557865c76ff2ab7d89933fc7
damak <akinlade.co@gmail.com>
2022-03-21 16:20:40 +0100

    export landing page from index file, css => scss

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,49 +1,40 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import BurgerAnimated from "../../components/BurgerAnimated/BurgerAnimated";
 
-import Classes from "./LandingPage.module.css";
+import Classes from "./LandingPage.module.scss";
 
-function Home() {
-  const navigate = useNavigate();
-
+function LandingPage() {
   return (
-    <div className={Classes.Home}>
-      <div className={Classes.Container}>
-        <BurgerAnimated />
-        <div className={Classes.TextContainer}>
-          <div className={Classes.Title}>
-            Burger Builder
-            <br />
-            <span>React App</span>
-          </div>
+    <div className={Classes.Container}>
+      <BurgerAnimated />
+      <div className={Classes.TextContainer}>
+        <div className={Classes.Title}>
+          Burger Builder
+          <br />
+          <span>React App</span>
+        </div>
 
-          <div className={Classes.Text}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit non
-            aliquid quidem minus deserunt et, qui laboriosam modi! Mollitia
-            asperiores placeat numquam qui error animi doloribus neque,
-            molestias exercitationem quis.
-            <br />
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit non
-            aliquid quidem minus deserunt et, qui laboriosam modi! Mollitia
-            asperiores placeat numquam qui error animi doloribus neque,
-            molestias exercitationem quis.
-          </div>
-          <div className={Classes.Buttons}>
-            <button onClick={() => navigate("app")}>View Live App</button>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                window.open("http://google.com", "_blank");
-              }}
-            >
-              View Repo
-            </button>
-          </div>
+        <div className={Classes.Text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit non
+          aliquid quidem minus deserunt et, qui laboriosam modi! Mollitia
+          asperiores placeat numquam qui error animi doloribus neque, molestias
+          exercitationem quis.
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit non
+          aliquid quidem minus deserunt et, qui laboriosam modi! Mollitia
+          asperiores placeat numquam qui error animi doloribus neque, molestias
+          exercitationem quis.
+        </div>
+        <div className={Classes.Buttons}>
+          <Link to="app">View Live App</Link>
+          <a href="http://google.com" target="_blank" rel="noreferrer">
+            View Repo
+          </a>
         </div>
       </div>
     </div>
   );
 }
 
-export default Home;
+export default LandingPage;
