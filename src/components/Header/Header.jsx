3bb793aa30b246efea1b8b89146e72e94b9e8cc2
damak <akinlade.co@gmail.com>
2022-06-23 03:47:54 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,28 @@
-import React from "react";
-import Logo from "../../assets/svgs/Logo";
-
-import Classes from "./Header.module.css";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Logo } from "../../assets/svg/icons";
+import Styles from "./Header.module.scss";
 
 function Header() {
+  const { user } = useSelector((state) => state.user);
+
   return (
-    <div className={Classes.Wrapper}>
-      <div className={Classes.Logo}>
-        <Logo />
+    <div className={Styles.Wrapper}>
+      <div className={Styles.Logo}>
+        <Link to="/">
+          <Logo />
+        </Link>
         <h3>Burger App</h3>
       </div>
-      <div className={Classes.Profile}>
+      <div className={Styles.Profile}>
         <img
           src="https://i.pinimg.com/736x/50/c9/b2/50c9b2e452527a88e18e22dff7a5a2ed.jpg"
           alt="avatar"
         />
-        <h4>Damak</h4>
+        <h4>
+          {user?.firstName && "Hi, "}
+          {user?.firstName || "Account"}
+        </h4>
       </div>
     </div>
   );
