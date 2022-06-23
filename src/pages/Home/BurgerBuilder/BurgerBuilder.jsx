ea3685f19e2c05cbe8b73e660d6933ecc57f4314
damak <akinlade.co@gmail.com>
2022-06-23 04:34:11 +0100

    burger builder

--- src/pages/Home/BurgerBuilder/BurgerBuilder.jsx
+++ src/pages/Home/BurgerBuilder/BurgerBuilder.jsx
@@ -1,39 +1,65 @@
-import React from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   increment,
   decrement,
   reset,
 } from "../../../store/features/burgerBuilderSlice";
+import { toast } from "react-toastify";
 import Burger from "../../../components/Burger";
 import BuildControls from "../../../components/BuildControls";
+import Modal from "../../../components/UI/Modal";
+import OrderSummary from "../../../components/OrderSummary";
 import Classes from "./BurgerBuilder.module.scss";
 
-function BurgerBuilder() {
-  const burgerIngredients = useSelector(
-    (state) => state.burgerBuilder.ingredients
-  );
-  const totalPrice = useSelector((state) => state.burgerBuilder.totalPrice);
+export default function BurgerBuilder() {
+  const [displayModal, setDisplayModal] = useState(false);
+  const { ingredients } = useSelector((state) => state.burgerBuilder);
+  const { totalPrice } = useSelector((state) => state.burgerBuilder);
+  const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-
+  // console.log(ingredients);
   function add(id) {
-    if (burgerIngredients[id].number === 5) {
+    if (ingredients[id].quantity === 3) {
       return;
     }
     dispatch(increment(id));
   }
 
   function remove(id) {
-    if (burgerIngredients[id].number === 0) {
+    if (ingredients[id].quantity === 0) {
       return;
     }
     dispatch(decrement(id));
   }
 
+  function order() {
+    const totalQuantity = Object.keys(ingredients)
+      .map((ingredient) => ingredients[ingredient].quantity)
+      .reduce((prev, curr) => prev + curr);
+
+    if (totalQuantity === 0) {
+      toast.warning("Start adding ingredients");
+    }
+
+    if (totalQuantity > 0 && !user) {
+      toast.warning("Please login to order");
+      return;
+    }
+
+    if (totalQuantity > 0) {
+      setDisplayModal(true);
+    }
+  }
+
+  function closeModal() {
+    setDisplayModal(false);
+  }
+
   return (
     <div className={Classes.Wrapper}>
       <div className={Classes.Burger}>
-        <Burger ingredients={burgerIngredients} />
+        <Burger ingredients={ingredients} />
       </div>
       <div className={Classes.Controls}>
         <BuildControls
@@ -41,10 +67,14 @@ function BurgerBuilder() {
           remove={(id) => remove(id)}
           totalPrice={totalPrice.toFixed(2)}
           reset={() => dispatch(reset())}
+          order={order}
         />
       </div>
+      {displayModal && (
+        <Modal onClick={closeModal}>
+          <OrderSummary onClick={closeModal}></OrderSummary>
+        </Modal>
+      )}
     </div>
   );
 }
-
-export default BurgerBuilder;
