a08093901a37e4d6dd64b13658b353a94abee7b3
damak <akinlade.co@gmail.com>
2022-06-23 04:01:09 +0100

    refactor

--- src/components/OrderCard/OrderCard.jsx
+++ src/components/OrderCard/OrderCard.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import Logo from "../../assets/svgs/Logo";
-import Classes from "./OrderCard.module.scss";
+import { Logo } from "../../assets/svg/icons";
+import Styles from "./OrderCard.module.scss";
 
 function OrderCard() {
   return (
-    <div className={Classes.Wrapper}>
-      <div className={Classes.Image}>
+    <div className={Styles.Wrapper}>
+      <div className={Styles.Image}>
         <Logo />
       </div>
-      <div className={Classes.Details}>
+      <div className={Styles.Details}>
         <h4>Order 356358683</h4>
         <p>
           QUANTITY: 1<span></span>
         </p>
         <p>$5.7</p>
       </div>
-      <div className={Classes.More}>
+      <div className={Styles.More}>
         <h5>24/02/22</h5>
       </div>
-      {/* <div className={Classes.Less}></div> */}
+      {/* <div className={Styles.Less}></div> */}
     </div>
   );
 }
