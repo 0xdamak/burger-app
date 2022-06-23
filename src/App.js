bf005f113994677252468f30c6936bfd6bf65761
damak <akinlade.co@gmail.com>
2022-06-23 02:27:32 +0100

    page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Live from "./pages/LiveApp/Live";
-import BurgerBuilder from "./pages/LiveApp/BurgerBuilder/BurgerBuilder";
-import Orders from "./pages/LiveApp/Orders/Orders";
-import Login from "./pages/Login/Login";
-
-import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
+import BurgerBuilder from "./pages/Home/BurgerBuilder";
+import Orders from "./pages/Orders";
+import Checkout from "./pages/Checkout";
+import Auth from "./pages/Auth";
+import Error from "./pages/Error";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="app" element={<Live />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="app" element={<Home />}>
           <Route path="" element={<BurgerBuilder />} />
           <Route path="orders" element={<Orders />} />
-          <Route path="login" element={<Login />} />
+          <Route path="login" element={<Auth />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="*" element={<Error />} />
         </Route>
-        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </div>
   );
