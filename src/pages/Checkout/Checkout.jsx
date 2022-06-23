3c053a8a5036cf51bc79693dbef61aa7c1ac25cf
damak <akinlade.co@gmail.com>
2022-06-23 04:30:50 +0100

    checkout page

---
+++ src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,11 @@
+import Styles from "./Checkout.module.scss";
+
+export default function Checkout() {
+  return (
+    <div className={Styles.Wrapper}>
+      <div className={Styles.Header}>
+        <h3>Checkout</h3>
+      </div>
+    </div>
+  );
+}
