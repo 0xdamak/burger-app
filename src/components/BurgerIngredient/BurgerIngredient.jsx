1061a1d0a23f3598e2d470730f9e57d16024df6b
damak <akinlade.co@gmail.com>
2022-06-23 03:42:00 +0100

    burger ingredient component

--- src/components/BurgerIngredient/BurgerIngredient.jsx
+++ src/components/BurgerIngredient/BurgerIngredient.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { motion } from "framer-motion";
 import TopBun from "../../assets/images/topBun.png";
 import BottomBun from "../../assets/images/bottomBun.png";
 import Cheese from "../../assets/images/cheese.png";
@@ -8,42 +8,120 @@ import Pear from "../../assets/images/pear.png";
 import Salad from "../../assets/images/salad.png";
 import Tomatoes from "../../assets/images/tomatoes.png";
 
-function BurgerIngredient({ ingredientType, className }) {
+function BurgerIngredient({ ingredientType, className, style }) {
   let ingredient;
 
   switch (ingredientType) {
     case "top-bun":
-      ingredient = <img src={TopBun} alt="top-bun" className={className} />;
+      ingredient = (
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.1 }}
+          src={TopBun}
+          style={style}
+          alt="top-bun"
+          className={className}
+        />
+      );
       break;
     case "bottom-bun":
       ingredient = (
-        <img src={BottomBun} alt="bottom-bun" className={className} />
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.1 }}
+          src={BottomBun}
+          style={style}
+          alt="bottom-bun"
+          className={className}
+        />
       );
       break;
     case "cheese":
-      ingredient = <img src={Cheese} alt="cheese" className={className} />;
+      ingredient = (
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.1 }}
+          src={Cheese}
+          style={style}
+          alt="cheese"
+          className={className}
+        />
+      );
       break;
     case "meat":
-      ingredient = <img src={Meat} alt="meat" className={className} />;
+      ingredient = (
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.1 }}
+          src={Meat}
+          style={style}
+          alt="meat"
+          className={className}
+        />
+      );
       break;
-    case "onions":
-      ingredient = <img src={Onions} alt="onions" className={className} />;
+    case "onion":
+      ingredient = (
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.1 }}
+          src={Onions}
+          style={style}
+          alt="onions"
+          className={className}
+        />
+      );
       break;
     case "pear":
-      ingredient = <img src={Pear} alt="pear" className={className} />;
+      ingredient = (
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.1 }}
+          src={Pear}
+          style={style}
+          alt="pear"
+          className={className}
+        />
+      );
       break;
     case "salad":
-      ingredient = <img src={Salad} alt="salad" className={className} />;
+      ingredient = (
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.2 }}
+          src={Salad}
+          style={style}
+          alt="salad"
+          className={className}
+        />
+      );
       break;
-    case "tomatoes":
-      ingredient = <img src={Tomatoes} alt="tomatoes" className={className} />;
+    case "tomato":
+      ingredient = (
+        <motion.img
+          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 4 }}
+          transition={{ duration: 0.1 }}
+          src={Tomatoes}
+          style={style}
+          alt="tomatoes"
+          className={className}
+        />
+      );
 
       break;
     default:
       ingredient = null;
   }
 
-  return <React.Fragment>{ingredient}</React.Fragment>;
+  return <>{ingredient}</>;
 }
 
 export default BurgerIngredient;
